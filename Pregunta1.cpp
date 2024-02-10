ba64c3010f2ae809ce519df3ac113aefb7fdf014
katiuskad <16-11108@usb.ve>
2024-02-09 23:42:52 -0400

    original

---
+++ Pregunta1.cpp
@@ -0,0 +1,23 @@
+md
+/***********************
+*
+* EXPRESIONES DIÁDICAS 1
+*
+**********************/
+#include <stdio.h>
+int main() {
+int x,y;
+for (;;) {
+printf("Introduzca el valor de x = ");
+scanf("%d",&x);
+printf("Introduzca el valor de y = ");
+scanf("%d",&y);
+printf("\n%d + %d = %d\n\n",x,y,x+y);
+}
+return 0;
+}
+/*
+*
+* printf("\n%d || %d = %d\n",x,y,x||y);
+*
+*/
