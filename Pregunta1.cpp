db93d53b39825e63c60d2f3b01144e7a26fd27db
katiuskasd <16-11108@usb.ve>
2024-02-10 00:27:08 -0400

    original

--- Pregunta1.cpp
+++ Pregunta1.cpp
@@ -1,21 +1,30 @@
 md
-/***********************
+/***
 *
-* EXPRESIONES DI�DICAS 1
+* EXPRESIONES DIï¿½DICAS 1
 *
-**********************/
+****/
+
 #include <stdio.h>
+
 int main() {
-int x,y;
-for (;;) {
-printf("Introduzca el valor de x = ");
-scanf("%d",&x);
-printf("Introduzca el valor de y = ");
-scanf("%d",&y);
-printf("\n%d + %d = %d\n\n",x,y,x+y);
+
+    int x,y;
+
+    for (;;) {
+
+    printf("Introduzca el valor de x = ");
+    scanf("%d",&x);
+    printf("Introduzca el valor de y = ");
+    scanf("%d",&y);
+    printf("\n%d || %d = %d\n",x,y,x||y);
+
+    }
+
+    return 0;
+
 }
-return 0;
-}
+
 /*
 *
 * printf("\n%d || %d = %d\n",x,y,x||y);
